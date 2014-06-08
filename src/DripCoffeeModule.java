d0a3382c2d1554ecc4279ce9cd73c5dd5b146125
Atsushi Iwasa <a-iwasa@biglobe.co.jp>
2014-06-08 20:33:40 +0900

    daggerのサンプルを追加

---
+++ src/DripCoffeeModule.java
@@ -0,0 +1,13 @@
+import dagger.Module;
+import dagger.Provides;
+import javax.inject.Singleton;
+
+@Module(
+    injects = CoffeeApp.class,
+    includes = PumpModule.class
+)
+class DripCoffeeModule {
+  @Provides @Singleton Heater provideHeater() {
+    return new ElectricHeater();
+  }
+}
---
+++ src/PumpModule.java
@@ -0,0 +1,9 @@
+import dagger.Module;
+import dagger.Provides;
+
+@Module(complete = false, library = true)
+class PumpModule {
+  @Provides Pump providePump(Thermosiphon pump) {
+    return pump;
+  }
+}
